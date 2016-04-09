a7a54bd5b33b08a1aed7e5f70c6f33e076617162
Sam <contactsamie@gmail.com>
2016-04-09 03:01:14 -0400

    updated nunit

--- AkkaPingPong.FSTests/when_it_gets_a_ping.fs
+++ AkkaPingPong.FSTests/when_it_gets_a_ping.fs
@@ -6,7 +6,6 @@ open AkkaPingPong.Core.Actors
 open AkkaPingPong.Core.Messages
 open AkkaPingPong.AkkaTestBase
 open NUnit.Framework
-open FsUnit
 open System
 
 [<TestFixture>]
@@ -20,7 +19,8 @@ type public when_it_gets_a_ping() =
         //Act
         new PingMessage() |> this.ActorSystem.LocateActor(typedefof<PingPongActor<_>>).Tell  |> ignore
         //Assert
-        this.AwaitAssert  (fun m->this.ExpectMsg<PingMessageCompleted>()|> ignore),TimeSpan.FromSeconds(5.0) |> ignore
+        this.AwaitAssert  (fun ()->this.ExpectMsg<PingMessageCompleted>() |> ignore),TimeSpan.FromSeconds(5.0) 
+        
 
      [<Test>]    
      member public  this.it_should_do_a_pong_integration() =
@@ -32,19 +32,19 @@ type public when_it_gets_a_ping() =
                                new PingMessage() |> this.ActorSystem.LocateActor(typedefof<PingPongActor<_>>).Tell 
                                )
         //Assert
-        this.AwaitAssert  (fun m->this.Subscriber.ExpectMsg<PongMessage>() |> ignore),TimeSpan.FromSeconds(20.0)|> ignore
+        this.AwaitAssert  (fun ()->this.Subscriber.ExpectMsg<PongMessage>() |> ignore),TimeSpan.FromSeconds(20.0)
 
      [<Test>]    
      member  public  this.it_should_do_a_pong_unit1() =
         //Arrange
         this.ActorSystem.CreateActor<PingPongActor<BlackHoleActor>>() |> ignore
         //Act
-        [1..10] |> List.iter (fun i->
+        [1..10] |> List.iter (fun (i)->
                                System.Threading.Thread.Sleep(1000) |> ignore
                                new PingMessage() |> this.ActorSystem.LocateActor(typedefof<PingPongActor<_>>).Tell
                                )
         //Assert
-        this.AwaitAssert  (fun m->this.ExpectMsg<PingMessageCompleted>()|> ignore),TimeSpan.FromSeconds(20.0) |> ignore
+        this.AwaitAssert  (fun m->this.ExpectMsg<PingMessageCompleted>() |> ignore),TimeSpan.FromSeconds(20.0) 
 
      [<Test>]    
      member public this.it_should_do_a_pong_unit2() =
@@ -53,4 +53,4 @@ type public when_it_gets_a_ping() =
         //Act
         new PingMessage() |> this.ActorSystem.LocateActor(typedefof<PingCoordinatorActor<_,_>>).Tell  |> ignore
         //Assert
-        this.AwaitAssert  (fun m->this.ExpectMsg<SorryImStashing>()|> ignore),TimeSpan.FromSeconds(5.0) |> ignore
+        this.AwaitAssert  (fun ()->this.ExpectMsg<SorryImStashing>() |> ignore),TimeSpan.FromSeconds(5.0) 
--- README.md
+++ README.md
@@ -1,14 +1,4 @@
 # AkkaPingPong
-    
-     [<Test>]     
-      member public  this.it_should_do_a_pong() =
-        //Arrange
-        this.ActorSystem.CreateActor<PingPongActor<BlackHoleActor>>()  |> ignore
-        //Act
-        new PingMessage() |> this.ActorSystem.LocateActor(typedefof<PingPongActor<_>>).Tell  |> ignore
-        //Assert
-        this.AwaitAssert  (fun m->this.ExpectMsg<PingMessageCompleted>()|> ignore),TimeSpan.FromSeconds(5.0) |> ignore
-
 
         [Test]
         public void it_should_do_a_pong()
