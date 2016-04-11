3822f7a630968e9fb777f3b0849f07aecdb2c6e6
Sam <contactsamie@gmail.com>
2016-04-10 22:02:48 -0400

    removing typedef thing

--- AkkaPingPong.FSTests/when_it_gets_a_ping.fs
+++ AkkaPingPong.FSTests/when_it_gets_a_ping.fs
@@ -1,4 +1,4 @@
-﻿namespace AkkaPingPong.FSTests
+namespace AkkaPingPong.FSTests
 
 open Akka.TestKit.TestActors
 open AkkaPingPong.ActorSystemLib
@@ -17,7 +17,7 @@ type public when_it_gets_a_ping() =
         //Arrange
         this.ActorSystem.CreateActor<PingPongActor<BlackHoleActor>>()  |> ignore
         //Act
-        new PingMessage() |> this.ActorSystem.LocateActor(typedefof<PingPongActor<_>>).Tell  |> ignore
+        new PingMessage() |> this.ActorSystem.LocateActor<PingPongActor<_>>().Tell  |> ignore
         //Assert
         let result = this.AwaitAssert  (fun ()->this.ExpectMsg<PingMessageCompleted>() |> ignore),TimeSpan.FromSeconds(5.0) 
         ()
@@ -29,7 +29,7 @@ type public when_it_gets_a_ping() =
         //Act
         [1..10] |> List.iter (fun i->
                                System.Threading.Thread.Sleep(1000) |> ignore
-                               new PingMessage() |> this.ActorSystem.LocateActor(typedefof<PingPongActor<_>>).Tell 
+                               new PingMessage() |> this.ActorSystem.LocateActor<PingPongActor<_>>().Tell 
                                )
         //Assert
         let result = this.AwaitAssert  (fun ()->this.Subscriber.ExpectMsg<PongMessage>() |> ignore),TimeSpan.FromSeconds(20.0)
@@ -42,7 +42,7 @@ type public when_it_gets_a_ping() =
         //Act
         [1..10] |> List.iter (fun i->
                                System.Threading.Thread.Sleep(1000) |> ignore
-                               new PingMessage() |> this.ActorSystem.LocateActor(typedefof<PingPongActor<_>>).Tell
+                               new PingMessage() |> this.ActorSystem.LocateActor<PingPongActor<_>>().Tell
                                )
         //Assert
         let result = this.AwaitAssert  (fun m->this.ExpectMsg<PingMessageCompleted>() |> ignore),TimeSpan.FromSeconds(20.0) 
@@ -53,7 +53,7 @@ type public when_it_gets_a_ping() =
         //Arrange
         this.ActorSystem.CreateActor<PingCoordinatorActor<AkkaTestBase.BlackHoleActor1, AkkaTestBase.BlackHoleActor2>>()  |> ignore
         //Act
-        new PingMessage() |> this.ActorSystem.LocateActor(typedefof<PingCoordinatorActor<_,_>>).Tell  |> ignore
+        new PingMessage() |> this.ActorSystem.LocateActor<PingCoordinatorActor<_,_>>().Tell  |> ignore
         //Assert
         let result = this.AwaitAssert  (fun ()->this.ExpectMsg<SorryImStashing>() |> ignore),TimeSpan.FromSeconds(5.0) 
         ()
